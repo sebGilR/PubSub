75bd2fd70dde5c83b4f806586b1c9e923a5ff9cb
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-19 19:01:10 -0500

    Add modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+import './style.scss';
+import { movies } from './modules/movies.js';
+import { actors } from './modules/actors.js';
 
+import { movieForm } from './modules/movie-form.js';
+import { actorForm } from './modules/actor-form.js';
+
+import { stats } from './modules/stats.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Done!')
+  let main = document.querySelector('main');
+  let aside = document.querySelector('aside');
+  //add a movies module
+  movies.render(main);
+
+  //add a form to add movies
+  movieForm.render(aside);
+
+  //add a stats module
+  stats.render(aside);
+
+  //add an actors module
+  actors.render(main);
+
+  //add a form to add actors
+  actorForm.render(aside);
 })
